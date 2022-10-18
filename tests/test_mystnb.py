db518433d23058b8cd6a0e9f84076f4ed8431efe
Kai Norman Clasen <kai.ce.tub@gmail.com>
2022-10-18 21:19:13 +0200

    Allow optional setting of tags
    - Add `hide-X` commands even though they are missing from the official documentation table
    - Add `code-prompt-show/hide` values even though they aren't really useful for the classic purpose

--- tests/test_mystnb.py
+++ tests/test_mystnb.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 import nbformat
 import pytest
 
@@ -44,6 +46,37 @@ def test_myst_nb_metadata_injector(prefix: str, remove_line: bool, delimiter: st
         assert len_source_lines == 2
     else:
         assert len_source_lines == 4
+
+
+# test extra tags configurator
+@pytest.mark.parametrize(
+    "inp_tags,extra_tags",
+    [
+        (["tag_inp1"], None),
+        (["tag_inp1", "tag_inp2"], None),
+        (["tag_inp1"], ["tag_inp1"]),
+        (["tag-inp1", "tag-inp2"], ["tag-inp1", "tag-inp2"]),
+    ],
+)
+def test_myst_nb_metadata_injector_extra_tags(
+    inp_tags: List[str], extra_tags: Union[List[str], None]
+):
+    # assumption: extra_tags always matches inp for all if not None
+    nb = nbformat.v4.new_notebook()
+    src_tmpl = """\
+        # {inp_tag}
+        code line
+    """
+    src_txt = "\n".join(src_tmpl.format(inp_tag=inp_tag) for inp_tag in inp_tags)
+    nb.cells.append(nbformat.v4.new_code_cell(src_txt))
+    new_nb = myst_nb_metadata_injector(
+        nbformat.writes(nb), prefix="#", remove_line=True, extra_tags=extra_tags
+    )
+    if extra_tags is not None:
+        assert new_nb.cells[0].metadata.hasattr("tags")
+        assert set(new_nb.cells[0].metadata.tags) == set(extra_tags)
+    else:
+        assert not new_nb.cells[0].metadata.hasattr("tags")
 
 
 def test_myst_nb_metadata_injector_syntax_sugar():
